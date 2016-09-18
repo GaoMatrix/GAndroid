3e766c5822efb7bba13f9adaa6603ea8277f9fc7
GaoMatrix <gcquan08@gmail.com>
2016-09-18 17:17:33 +0800

    添加material-dialogs的测试

--- BottomTablayout/README.md
+++ BottomTablayout/README.md
@@ -1,1 +1,7 @@
+### BottonTabLayout
 http://www.jianshu.com/p/60096b338dd2
+
+
+### Material
+[material-dialogs](https://github.com/afollestad/material-dialogs)
+[MaterialDrawer](https://github.com/mikepenz/MaterialDrawer)
--- BottomTablayout/src/main/java/com/gao/tabview/WechatFragment.java
+++ BottomTablayout/src/main/java/com/gao/tabview/WechatFragment.java
@@ -1,14 +1,32 @@
 package com.gao.tabview;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.GravityEnum;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by yx on 16/4/3.
  */
-public class WechatFragment extends BaseFragment implements ITabClickListener{
+public class WechatFragment extends BaseFragment implements ITabClickListener {
+
+
+    @BindView(R.id.showColorChooserCustomColors)
+    Button mShowColorChooserCustomColors;
+    @BindView(R.id.showThemed)
+    Button mShowThemed;
+
     @Override
     public void fetchData() {
 
@@ -27,6 +45,7 @@ public class WechatFragment extends BaseFragment implements ITabClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.wechat_layout, container, false);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -38,5 +57,33 @@ public class WechatFragment extends BaseFragment implements ITabClickListener{
     @Override
     public BaseFragment getFragment() {
         return this;
+    }
+
+
+    @OnClick({R.id.showColorChooserCustomColors, R.id.showThemed})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.showColorChooserCustomColors:
+                break;
+            case R.id.showThemed:
+                new MaterialDialog.Builder(getContext())
+                        .title(R.string.useGoogleLocationServices)
+                        .content(R.string.useGoogleLocationServicesPrompt)
+                        .positiveText(R.string.agree)
+                        .negativeText(R.string.disagree)
+                        .positiveColorRes(R.color.material_red_400)
+                        .negativeColorRes(R.color.material_red_400)
+                        .titleGravity(GravityEnum.CENTER)
+                        .titleColorRes(R.color.material_red_400)
+                        .contentColorRes(android.R.color.white)
+                        .backgroundColorRes(R.color.material_blue_grey_800)
+                        .dividerColorRes(R.color.accent)
+                        .btnSelector(R.drawable.md_btn_selector_custom, DialogAction.POSITIVE)
+                        .positiveColor(Color.WHITE)
+                        .negativeColorAttr(android.R.attr.textColorSecondaryInverse)
+                        .theme(Theme.DARK)
+                        .show();
+                break;
+        }
     }
 }
