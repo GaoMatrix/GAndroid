3272a29ece9241b52853a149dc909b087ae147bc
GaoMatrix <gcquan08@gmail.com>
2016-09-19 14:58:39 +0800

    添加底部不可以滑动的Tablayout

--- BottomTablayout/src/main/java/com/gao/tabview/tablayout/TabLayoutBottomFragment.java
+++ BottomTablayout/src/main/java/com/gao/tabview/tablayout/TabLayoutBottomFragment.java
@@ -1,18 +1,71 @@
 package com.gao.tabview.tablayout;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
 import com.gao.tabview.R;
+import com.gao.tabview.tablayout.entity.TabEntity;
+
+import java.util.ArrayList;
 
 /**
  * Created by GaoMatrix on 2016/9/19.
  * 底部Switch Fragment 只能点击tab切换
  */
-public class TabLayoutBottomFragment extends Activity {
+public class TabLayoutBottomFragment extends AppCompatActivity {
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<Fragment> mFragments2 = new ArrayList<>();
+
+    private String[] mTitles = {"首页", "消息", "联系人", "更多"};
+    private int[] mIconUnselectIds = {
+            R.mipmap.tab_home_unselect, R.mipmap.tab_speech_unselect,
+            R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.tab_home_select, R.mipmap.tab_speech_select,
+            R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private CommonTabLayout mTabLayout_3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout_bottom_fragment);
+
+        for (String title : mTitles) {
+            mFragments.add(SimpleCardFragment.getInstance("Switch ViewPager " + title));
+            mFragments2.add(SimpleCardFragment.getInstance("Switch Fragment " + title));
+        }
+
+
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+
+        /** with Fragments */
+        mTabLayout_3 = (CommonTabLayout) findViewById(R.id.tl_3);
+
+        mTabLayout_3.setTabData(mTabEntities, this, R.id.fl_change, mFragments2);
+
+        mTabLayout_3.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+
+            }
+        });
+        mTabLayout_3.setCurrentTab(1);
+
+        //显示未读红点
+        mTabLayout_3.showDot(1);
     }
+
 }
