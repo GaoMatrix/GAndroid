e38af69a3a513c3d1fbc2b6d5d0087aacb83d3f1
GaoMatrix <gcquan08@gmail.com>
2016-09-21 17:40:54 +0800

    添加欢迎引导页面

---
+++ ToolBar/README.md
@@ -0,0 +1,16 @@
+### FlycoTabLayout
+https://github.com/H07000223/FlycoTabLayout
+
+
+### BGABanner-Android
+引导界面滑动导航 + 大于等于1页时无限轮播 + 各种切换动画轮播效果
+https://github.com/bingoogolapple/BGABanner-Android
+
+
+### BGARefreshLayout-Android
+多种下拉刷新效果、上拉加载更多、可配置自定义头部广告位
+https://github.com/bingoogolapple/BGARefreshLayout-Android
+
+
+
+
---
+++ ToolBar/src/main/java/com/gao/toolbar/welcome/WelcomeActivity.java
@@ -0,0 +1,118 @@
+package com.gao.toolbar.welcome;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gao.toolbar.MainActivity;
+import com.gao.toolbar.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.bingoogolapple.bgabanner.BGABanner;
+import cn.bingoogolapple.bgabanner.BGABannerUtil;
+
+/**
+ * Created by GaoMatrix on 2016/9/21.
+ */
+public class WelcomeActivity extends Activity implements View.OnClickListener {
+    private static final String TAG = WelcomeActivity.class.getSimpleName();
+    private TextView mSkipTv;
+    private Button mEnterBtn;
+    private BGABanner mBackgroundBanner;
+    private BGABanner mForegroundBanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+        setListener();
+        processLogic();
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_welcome);
+        mSkipTv = (TextView) findViewById(R.id.tv_guide_skip);
+        mEnterBtn = (Button) findViewById(R.id.btn_guide_enter);
+        mBackgroundBanner = (BGABanner) findViewById(R.id.banner_guide_background);
+        mForegroundBanner = (BGABanner) findViewById(R.id.banner_guide_foreground);
+    }
+
+    private void setListener() {
+        mSkipTv.setOnClickListener(this);
+        mEnterBtn.setOnClickListener(this);
+
+        // 监听页码切换事件，控制「跳过按钮」和「进入主界面的显示与隐藏」
+        mForegroundBanner.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position == mForegroundBanner.getItemCount() - 2) {
+                    ViewCompat.setAlpha(mEnterBtn, positionOffset);
+                    ViewCompat.setAlpha(mSkipTv, 1.0f - positionOffset);
+
+                    if (positionOffset > 0.5f) {
+                        mEnterBtn.setVisibility(View.VISIBLE);
+                        mSkipTv.setVisibility(View.GONE);
+                    } else {
+                        mEnterBtn.setVisibility(View.GONE);
+                        mSkipTv.setVisibility(View.VISIBLE);
+                    }
+                } else if (position == mForegroundBanner.getItemCount() - 1) {
+                    mSkipTv.setVisibility(View.GONE);
+                    mEnterBtn.setVisibility(View.VISIBLE);
+                    ViewCompat.setAlpha(mEnterBtn, 1.0f);
+                } else {
+                    mSkipTv.setVisibility(View.VISIBLE);
+                    ViewCompat.setAlpha(mSkipTv, 1.0f);
+                    mEnterBtn.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void processLogic() {
+        mBackgroundBanner.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        mForegroundBanner.setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+        // 初始化方式1：通过传入数据模型并结合Adapter的方式初始化
+        mBackgroundBanner.setAdapter(new BGABanner.Adapter() {
+            @Override
+            public void fillBannerItem(BGABanner banner, View view, Object model, int position) {
+                ((ImageView) view).setImageResource((int) model);
+            }
+        });
+        mBackgroundBanner.setData(Arrays.asList(R.drawable.uoko_guide_background_1, R.drawable.uoko_guide_background_2, R.drawable.uoko_guide_background_3), null);
+
+
+        // 初始化方式2：通过直接传入视图集合的方式初始化
+        List<View> views = new ArrayList<>();
+        views.add(BGABannerUtil.getItemImageView(this, R.drawable.uoko_guide_foreground_1));
+        views.add(BGABannerUtil.getItemImageView(this, R.drawable.uoko_guide_foreground_2));
+        views.add(BGABannerUtil.getItemImageView(this, R.drawable.uoko_guide_foreground_3));
+        mForegroundBanner.setData(views);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_guide_enter || view.getId() == R.id.tv_guide_skip) {
+            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 在界面可见时给背景Banner设置一个白色背景，避免滑动过程中两个Banner都设置透明度后能看到Launcher
+        mBackgroundBanner.setBackgroundResource(android.R.color.white);
+    }
+}
